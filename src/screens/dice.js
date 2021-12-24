82c689633666ee96064be7c9557c3b88a1ad55cc
timur kazdayev <tkazdayev@gmail.com>
2021-12-24 08:59:03 +0600

    edit dice.js
    add roll button and name in top right conner

--- src/screens/dice.js
+++ src/screens/dice.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react';
-// import { React } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component}  from "react";
+import {View, Text, StyleSheet, Button, Image} from "react-native";
 
-export default class Dice extends Component{
-    render(){
+export default class Dice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            source:require('../../assets/dice1.png')
+        }
+    }
+    
+    buttonHandler=()=>{
+        let array=[require('../../assets/dice1.png'),require('../../assets/dice2.png'),
+            require('../../assets/dice3.png'),require('../../assets/dice4.png'),
+            require('../../assets/dice5.png'),require('../../assets/dice6.png')]
+        let number=Math.floor(Math.random() * 6);
+        this.setState({source:array[number]})
+    }
+
+    render() {
         return (
-            <View>
-                <View>
-                    <Text>Let's play?</Text>
-                </View>
-                <View>
-                    <Text>p1</Text>
-                    
-                </View>
-                <View>
-                    <Text>p2</Text>
-                </View>
+            <View style={localStyles.body}>
+                <Text style={localStyles.name}>Timur Kazdayev SE-2017</Text>
+                <Image source={this.state.source} style={localStyles.image}/>
+                <Button title='ROLL' onPress={this.buttonHandler}/>
             </View>
 
-        );
+        )
     }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const localStyles= StyleSheet.create({
+    body:{
+        backgroundColor: '#fff',
+        top:20
+    },
+    name:{
+        position:'absolute',
+        right:5,
+        top:20
+    },
+    image:{
+        width:200,
+        height:200
+    }
+})
